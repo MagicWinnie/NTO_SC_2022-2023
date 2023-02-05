dd4135b3664c177febab29d51528d600459ce5f5
MagicWinnie <magicwinnie@yandex.ru>
2023-02-05 15:13:38 +0300

    optimizied

--- additional/check_patterns_powers.py
+++ additional/check_patterns_powers.py
@@ -1,5 +1,6 @@
-from typing import List, Tuple
-
+from itertools import product
+from typing import Dict, List, Tuple
+import json
 
 class Enemy:
     def __init__(self, hp: int, dmg: int, heading: int, position: int):
@@ -61,99 +62,89 @@ def simulation(answer: str) -> Tuple[bool, int, int]:
             raise Exception('Unknown structure')
 
     points = 0
-    enemies: List[Enemy] = []
+    enemy = Enemy(int(1e9), 1, 0, 0)
     #only one healthy but weak enemy
-    enemies.append(Enemy(1e9, 1, 0, 0))
     damage = 0
-    for _ in range(10000):
+    while True:
         # 1. move all enemies
-        for enemy in enemies:
-            if enemy.heading == 0:
-                if enemy.position + 1 < 20:
-                    if structures[enemy.position + 1].name not in ('base', 'fence', 'cannon', 'tower'):
-                        enemy.position += 1
-            elif enemy.heading == 1:
-                if enemy.position - 1 >= 0:
-                    if structures[enemy.position - 1].name not in ('base', 'fence', 'cannon', 'tower'):
-                        enemy.position -= 1
-        # 2. make damage to player's structures
-        for enemy in enemies:
+        if enemy.heading == 0:
             if enemy.position + 1 < 20:
-                structures[enemy.position + 1].hp -= enemy.dmg
-                if structures[enemy.position + 1].hp <= 0:
-                    if structures[enemy.position + 1].name == "base":
-                        return False, points
-                    else:
-                        structures[enemy.position + 1] = Structre('free')
+                if structures[enemy.position + 1].name not in ('base', 'fence', 'cannon', 'tower'):
+                    enemy.position += 1
+            elif enemy.position + 1 >= 20:
+                return True, points, damage
+        elif enemy.heading == 1:
             if enemy.position - 1 >= 0:
-                structures[enemy.position - 1].hp -= enemy.dmg
-                if structures[enemy.position - 1].hp <= 0:
-                    if structures[enemy.position - 1].name == "base":
-                        return False, points
-                    else:
-                        structures[enemy.position - 1] = Structre('free')
+                if structures[enemy.position - 1].name not in ('base', 'fence', 'cannon', 'tower'):
+                    enemy.position -= 1
+        # 2. make damage to player's structures
+        if enemy.position + 1 < 20:
+            structures[enemy.position + 1].hp -= enemy.dmg
+            if structures[enemy.position + 1].hp <= 0:
+                if structures[enemy.position + 1].name == "base":
+                    return False, points, damage
+                else:
+                    structures[enemy.position + 1] = Structre('free')
+        if enemy.position - 1 >= 0:
+            structures[enemy.position - 1].hp -= enemy.dmg
+            if structures[enemy.position - 1].hp <= 0:
+                if structures[enemy.position - 1].name == "base":
+                    return False, points, damage
+                else:
+                    structures[enemy.position - 1] = Structre('free')
         # 3. new enemies appear
         #remove apperaing stage
         #enemies.append(Enemy(hp, dmg, 0, 0))
         #enemies.append(Enemy(hp, dmg, 1, 19))
         # 4. make damage to enemy by trap
-        for enemy in enemies:
-            if structures[enemy.position].name == "trap":
-                enemy.hp -= structures[enemy.position].dmg
-                damage += structures[enemy.position].dmg
-        i = 0
-        while i < len(enemies):
-            if enemies[i].hp <= 0:
-                del enemies[i]
-            else:
-                i += 1
+        if structures[enemy.position].name == "trap":
+            enemy.hp -= structures[enemy.position].dmg
+            damage += structures[enemy.position].dmg
         # 5. make damage to enemy by cannon/tower
-        for enemy in enemies:
-            if enemy.position - 4 >= 0:
-                if structures[enemy.position - 4].name == "tower":
-                    enemy.hp -= structures[enemy.position - 4].dmg
-                    damage += structures[enemy.position - 4].dmg
-            if enemy.position - 3 >= 0:
-                if structures[enemy.position - 3].name == "tower":
-                    enemy.hp -= structures[enemy.position - 3].dmg
-                    damage += structures[enemy.position - 3].dmg
-            if enemy.position - 2 >= 0:
-                if structures[enemy.position - 2].name in ("cannon", "tower"):
-                    enemy.hp -= structures[enemy.position - 2].dmg
-                    damage += structures[enemy.position - 2].dmg
-            if enemy.position - 1 >= 0:
-                if structures[enemy.position - 1].name in ("cannon", "tower"):
-                    enemy.hp -= structures[enemy.position - 1].dmg
-                    damage += structures[enemy.position - 1].dmg
-            if enemy.position + 1 < 20:
-                if structures[enemy.position + 1].name in ("cannon", "tower"):
-                    enemy.hp -= structures[enemy.position + 1].dmg
-                    damage += structures[enemy.position +1].dmg
-            if enemy.position + 2 < 20:
-                if structures[enemy.position + 2].name in ("cannon", "tower"):
-                    enemy.hp -= structures[enemy.position + 2].dmg
-                    damage += structures[enemy.position +2].dmg
-            if enemy.position + 3 < 20:
-                if structures[enemy.position + 3].name == "tower":
-                    enemy.hp -= structures[enemy.position + 3].dmg
-                    damage += structures[enemy.position + 3].dmg
-            if enemy.position + 4 < 20:
-                if structures[enemy.position + 4].name == "tower":
-                    enemy.hp -= structures[enemy.position + 4].dmg
-                    damage += structures[enemy.position + 4].dmg
-        i = 0
-        while i < len(enemies):
-            if enemies[i].hp <= 0:
-                del enemies[i]
-            else:
-                i += 1
+        if enemy.position - 4 >= 0:
+            if structures[enemy.position - 4].name == "tower":
+                enemy.hp -= structures[enemy.position - 4].dmg
+                damage += structures[enemy.position - 4].dmg
+        if enemy.position - 3 >= 0:
+            if structures[enemy.position - 3].name == "tower":
+                enemy.hp -= structures[enemy.position - 3].dmg
+                damage += structures[enemy.position - 3].dmg
+        if enemy.position - 2 >= 0:
+            if structures[enemy.position - 2].name in ("cannon", "tower"):
+                enemy.hp -= structures[enemy.position - 2].dmg
+                damage += structures[enemy.position - 2].dmg
+        if enemy.position - 1 >= 0:
+            if structures[enemy.position - 1].name in ("cannon", "tower"):
+                enemy.hp -= structures[enemy.position - 1].dmg
+                damage += structures[enemy.position - 1].dmg
+        if enemy.position + 1 < 20:
+            if structures[enemy.position + 1].name in ("cannon", "tower"):
+                enemy.hp -= structures[enemy.position + 1].dmg
+                damage += structures[enemy.position +1].dmg
+        if enemy.position + 2 < 20:
+            if structures[enemy.position + 2].name in ("cannon", "tower"):
+                enemy.hp -= structures[enemy.position + 2].dmg
+                damage += structures[enemy.position +2].dmg
+        if enemy.position + 3 < 20:
+            if structures[enemy.position + 3].name == "tower":
+                enemy.hp -= structures[enemy.position + 3].dmg
+                damage += structures[enemy.position + 3].dmg
+        if enemy.position + 4 < 20:
+            if structures[enemy.position + 4].name == "tower":
+                enemy.hp -= structures[enemy.position + 4].dmg
+                damage += structures[enemy.position + 4].dmg
         # 6. add point if all ok
         points += 1
-        if (enemies[0].position >= 20):
-            return True, points, damage
-    return True, points, damage
 
-
-if __name__ == "__main__":
-    print(simulation("========RFC========="))
-
+alphabet = 'CFRT'
+comb_dmg: Dict[str, int] = {}
+for r in range(2, 6):
+    perms = list(product(alphabet, repeat=r)) # type: ignore
+    for perm in perms:
+        structure = ''.join(perm)
+        answer = '=' + structure + '=' * (20 - 1 - len(structure))
+        comb_dmg[structure] = simulation(answer)[2]
+    print(r)
+comb_dmg = {k: v for k, v in sorted(comb_dmg.items(), key=lambda item: item[1], reverse=True)}
+with open('output.json', 'w') as outfile:
+    json.dump(comb_dmg, outfile, indent=4)
