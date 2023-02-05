3a1b390f906e1ce2ba24cdab662d7d3c956013f7
MagicWinnie <magicwinnie@yandex.ru>
2023-02-05 11:52:19 +0300

    changes to check

--- additional/check.py
+++ additional/check.py
@@ -4,4 +4,6 @@ from simulation import simulation
 hp, dmg = map(int, input().split())
 base, fence, trap, cannon, tower = map(int, input().split())
 
-print(simulation(hp, dmg, base, fence, trap, cannon, tower, solution(hp, dmg, base, fence, trap, cannon, tower)))
+answer = solution(hp, dmg, base, fence, trap, cannon, tower)
+print(simulation(hp, dmg, base, fence, trap, cannon, tower, answer))
+print(answer)
